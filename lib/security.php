803b6b3a5d5f28e8d591b54a6ed8648b74ad6743
Nobuo OOHARA <oohara@e-2.co.jp>
2025-06-25 14:45:12 +0900

    meta name=’robots’ content=’max-image-preview:large’ の削除

--- lib/security.php
+++ lib/security.php
@@ -87,6 +87,9 @@ remove_filter('the_content_feed', 'wp_staticize_emoji');
 remove_filter('comment_text_rss', 'wp_staticize_emoji');
 remove_filter('wp_mail', 'wp_staticize_emoji_for_email');
 
+// meta name=’robots’ content=’max-image-preview:large’ の削除
+remove_filter( 'wp_robots', 'wp_robots_max_image_preview_large' );
+
 // xmlrpc.phpの無効化
 add_filter('xmlrpc_enabled', '__return_false');
 
